7e0fba31d7b91515acb72c0082d4f6fad1916a73
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-27 00:12:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song
-  attr_accessor :title, :name
+  attr_accessor :title
 
   def title=(title)
     @title = title
   end
 
-  def artist=(name)
-    @name = name
+  def artist=(title)
+    @title = title
   end
 end
